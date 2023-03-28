4e78ace4c7f115d9dc9faab6b2084636fcb03a23
XEVON <87572659+XEVON01@users.noreply.github.com>
2023-03-28 18:29:13 +0530

    Update percentage calculator.py

--- percentage calculator.py
+++ percentage calculator.py
@@ -1,4 +1,4 @@
-y_v=int(input("your value"))
-t_v=int(input("total value"))
-prcnt=(y_v/t_v)*100
+y_v=int(input("your value")) #the value/amount you want to enter, y_v: your value
+t_v=int(input("total value")) #the total value/amount you want to enter, t_v: total value
+prcnt=(y_v/t_v)*100 #formula of percentage
 print (prcnt)
